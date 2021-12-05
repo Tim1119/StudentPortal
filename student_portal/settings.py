966b19abd4051fb4952ac10070f31a4eeb280433
Ajani Timothy <timothyinioluwa@gmail.com>
2021-12-05 05:43:03 +0100

    file

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -147,7 +147,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
