2c83f35b4ed2c9a5b8432a3ca1fa6bfada3b6f3a
Ajani Timothy <timothyinioluwa@gmail.com>
2021-12-05 10:02:18 +0100

    a

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -14,8 +14,10 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG=True
 
 ALLOWED_HOSTS = ['digi-students.herokuapp.com']
 
